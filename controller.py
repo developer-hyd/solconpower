b8d39b059c88bbe6be0457913bf2d7684527fc91
vivek-munjewar <vivek.munjewar@oildex.com>
2017-05-15 15:47:09 +0530

    initial commit

---
+++ controller.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask, request, render_template, url_for, flash, \
+    redirect,session
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+@app.route('/products')
+def products():
+   return render_template('products.html')
+
+@app.route('/contact',methods=['GET','POST'])
+def contact():
+	if request.method=='POST':
+		name = request.form['name']
+		email = request.form['email']
+		message = request.form['message']
+		print (name+message+email)
+		flash('Our Expert Will Get Back To You Soon........!')
+		return redirect(request.url)
+	return render_template('contact.html')
+
+@app.route('/products/SolarMeter')
+def solarMeter():
+   return render_template('index.html')         
+
+@app.route('/products/RoofTop')
+def rooftop():
+   return render_template('RoofTop.html') 
+
+@app.route('/products/WaterPump')
+def waterpump():
+   return render_template('WaterPump.html')    
+
+@app.route('/products/SolarLight')
+def solarlight():
+   return render_template('SolarLight.html')   
+
+if __name__ == '__main__':
+    app.run(debug='true',port=8000)    
